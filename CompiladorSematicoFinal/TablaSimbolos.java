92c23b0eca91ba6ca3fd6028451302d3e5dd62f1
Lowellq <lowell.quinonez@gmail.com>
2019-03-17 22:58:42 -0700

    Version mas estable
    Version con fallas en la tabla de simbolo

---
+++ CompiladorSematicoFinal/TablaSimbolos.java
@@ -0,0 +1,48 @@
+package CompiladorSematicoFinal;
+
+
+public class TablaSimbolos {
+
+	String nombre;
+	String tipo;
+	String valor;
+	int posicion;
+	
+	
+	public TablaSimbolos(String nombre, String tipo, String valor, int posicion) {
+		super();
+		this.nombre = nombre;
+		this.tipo = tipo;
+		this.valor = valor;	
+		this.posicion = posicion;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getValor() {
+		return valor;
+	}
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+	
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	public int getposicion() {
+		return posicion;
+	}
+	public void setposicion(int posicion) {
+		this.posicion = posicion;
+	}
+
+
+	
+}
